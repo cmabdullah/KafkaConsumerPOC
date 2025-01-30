df2586b7ae416d19ee9f23fb6889990633453ddd
Khan, C M Abdullah <cmabdullah21@gmail.com>
2025-01-31 01:06:20 +0600

    update dependency

--- src/main/java/org/cm/kafka/KafkaMessageConsumerClient.java
+++ src/main/java/org/cm/kafka/KafkaMessageConsumerClient.java
@@ -3,7 +3,7 @@ package org.cm.kafka;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
 
-@KafkaListener(groupId = "message-app", uniqueGroupId = true)
+@KafkaListener(groupId = "message-app", uniqueGroupId = true, uniqueGroupIdDeleteOnShutdown = true)
 public class KafkaMessageConsumerClient {
 	@Topic("user-online")
 	void consumeEvent(String message) {
